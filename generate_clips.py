1f25e03d3e5880bb17df64ee4b3a3372f10eaed9
Brian Foo <brian@youaremyjoy.org>
2016-10-28 18:24:58 -0400

    add names to json file, clip lines

--- generate_clips.py
+++ generate_clips.py
@@ -19,6 +19,7 @@ parser.add_argument('-min', dest="MIN_DURATION", type=float, default=0.1, help="
 parser.add_argument('-pa', dest="PAD", type=float, default=0.02, help="Amount of seconds to pad before and after")
 parser.add_argument('-fa', dest="FADE", type=float, default=0.02, help="Amount of seconds to fade before and after")
 parser.add_argument('-o', dest="OVERRIDE", type=int, default=0, help="Override existing syllable data")
+parser.add_argument('-fe', dest="FILE_EXT", default=".wav", help="File extension of audio clips")
 
 # init input
 args = parser.parse_args()
@@ -26,7 +27,8 @@ MIN_DURATION = args.MIN_DURATION
 PAD = args.PAD
 FADE = args.FADE
 OVERRIDE = args.OVERRIDE
-types = ["words", "syllables", "nonwords", "pauses"]
+FILE_EXT = args.FILE_EXT
+types = ["lines", "words", "syllables", "nonwords", "pauses"]
 
 # create directories
 if not os.path.exists(args.OUTPUT_DIR):
@@ -40,38 +42,82 @@ with open(args.INPUT_FILE) as f:
     data = json.load(f)
 
 clips = []
+lines = data["lines"]
 words = data["words"]
 nonwords = data["nonwords"]
 pauses = data["pauses"]
 
-def getName(prefix, i, obj):
-    return "%s_%s_%s_%s" % (prefix, str(i).zfill(3), re.sub(r'\W+', '', word["alignedWord"], start))
+# add names
+
+changed = False
+
+for i, line in enumerate(lines):
+    name = "line_%s_%s" % (str(i).zfill(3), int(line["start"]))
+    if "name" not in line or line["name"] != name:
+        changed = True
+    lines[i]["name"] = name
+
 
 for i, word in enumerate(words):
-    start = int(word["start"])
-    name = "word_%s_%s_%s" % (str(i).zfill(3), re.sub(r'\W+', '', word["alignedWord"]), start)
-    word.update({"name": name, "type": "words"})
+    name = "word_%s_%s_%s" % (str(i).zfill(3), re.sub(r'\W+', '', word["alignedWord"]), int(word["start"]))
+    words[i]["name"] = name
+    if "name" not in line or word["name"] != name:
+        changed = True
+    for j, syllable in enumerate(word["syllables"]):
+        name = "syll_%s_%s_%s_%s" % (str(i).zfill(3), j, re.sub(r'\W+', '', syllable["text"]), int(syllable["start"]))
+        if "name" not in syllable or syllable["name"] != name:
+            changed = True
+        words[i]["syllables"][j]["name"] = name
+
+for i, word in enumerate(nonwords):
+    name = "nonword_%s_%s" % (str(i).zfill(3), int(word["start"]))
+    if "name" not in line or word["name"] != name:
+        changed = True
+    nonwords[i]["name"] = name
+
+for i, word in enumerate(pauses):
+    name = "pause_%s_%s" % (str(i).zfill(3), int(word["start"]))
+    if "name" not in line or word["name"] != name:
+        changed = True
+    pauses[i]["name"] = name
+
+# update file with names
+
+if changed:
+    data["lines"] = lines
+    data["words"] = words
+    data["nonwords"] = nonwords
+    data["pauses"] = pauses
+    with open(args.INPUT_FILE, 'w') as f:
+        json.dump(data, f, indent=2)
+    print "Updated names in file."
+
+# create clips
+
+for i, line in enumerate(lines):
+    line.update({"type": "lines"})
+    clips.append(line)
+
+for i, word in enumerate(words):
+    word.update({"type": "words"})
     clips.append(word)
     for j, syllable in enumerate(word["syllables"]):
-        start = int(syllable["start"])
-        name = "syll_%s_%s_%s_%s" % (str(i).zfill(3), j, re.sub(r'\W+', '', syllable["text"]), start)
-        syllable.update({"name": name, "type": "syllables"})
+        syllable.update({"type": "syllables"})
         clips.append(syllable)
 
 for i, word in enumerate(nonwords):
-    start = int(word["start"])
-    name = "nonword_%s_%s" % (str(i).zfill(3), start)
-    word.update({"name": name, "type": "nonwords"})
+    word.update({"type": "nonwords"})
     clips.append(word)
 
 for i, word in enumerate(pauses):
-    start = int(word["start"])
-    name = "pause_%s_%s" % (str(i).zfill(3), start)
-    word.update({"name": name, "type": "pauses"})
+    word.update({"type": "pauses"})
     clips.append(word)
 
+# generate clips
+
 for i, clip in enumerate(clips):
-    fname = args.OUTPUT_DIR + clip["type"] + '/' + clip["name"] + '.wav'
+    fname = args.OUTPUT_DIR + clip["type"] + '/' + clip["name"] + FILE_EXT
+    fnameTmp = args.OUTPUT_DIR + clip["type"] + '/' + clip["name"] + "_temp" + FILE_EXT
     if os.path.isfile(fname) and not OVERRIDE:
         continue
     start = max(round(clip["start"] - PAD, 2), 0)
@@ -79,12 +125,18 @@ for i, clip in enumerate(clips):
     dur = end - start
     if dur < MIN_DURATION:
         end = round(start + MIN_DURATION, 2)
+
     # cut the clip
-    command = ['ffmpeg', '-i', args.INPUT_AUDIO_FILE, '-ss', str(start), '-to', str(end), '-c', 'copy', fname, '-y']
+    command = ['ffmpeg', '-i', args.INPUT_AUDIO_FILE, '-ss', str(start), '-to', str(end), '-c', 'copy', fnameTmp, '-y']
     # print " ".join(command)
     finished = subprocess.check_call(command)
+
     # fade the clip
     st = round(end - start - FADE, 2)
-    command = ['ffmpeg', '-i', fname, '-af', "afade=t=in:ss=0:d="+str(FADE)+",afade=t=out:st="+str(st)+":d="+str(FADE), fname, '-y']
+    command = ['ffmpeg', '-i', fnameTmp, '-af', "afade=t=in:ss=0:d="+str(FADE)+",afade=t=out:st="+str(st)+":d="+str(FADE), fname, '-y']
     # print " ".join(command)
+    finished = subprocess.check_call(command)
+
+    # delete temp file
+    command = ['rm', fnameTmp]
     subprocess.call(command)
